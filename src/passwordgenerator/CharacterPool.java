ed0afedbb9a908e4a428cb63b7a7c753835a8e61
Дмитрий Горбов <ddk84ejes4@gmail.com>
2026-09-15 18:05:37 +0300

    Создан скелет класса пула символов для генерации пароля.

--- src/passwordgenerator/CharacterPool.java
+++ src/passwordgenerator/CharacterPool.java
@@ -1,4 +1,19 @@
 package passwordgenerator;
 
 public class CharacterPool {
+
+    private static final String LETTERS_LOWER = "abcdefghijklmnopqrstuvwxyz";
+    private static final String LETTERS_UPPER = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final String NUMBERS = "1234567890";
+    private static final String SPECIAL_SYMBOLS = "!@#$%^&*()-_=+[]{}";
+
+    private final String pool;
+
+    public CharacterPool(PasswordConfig config) {}
+
+    public String getPool() {
+        return this.pool;
+    }
+
+
 }
--- src/passwordgenerator/PasswordConfig.java
+++ src/passwordgenerator/PasswordConfig.java
@@ -1,4 +1,5 @@
 package passwordgenerator;
 
 public class PasswordConfig {
+
 }
