14508560add95e856ee0944dff656668a4bdddf5
Дмитрий Горбов <ddk84ejes4@gmail.com>
2026-09-17 11:46:44 +0300

    Реализована логика Main.

--- src/passwordgenerator/Main.java
+++ src/passwordgenerator/Main.java
@@ -2,7 +2,10 @@ package passwordgenerator;
 
 public class Main {
     public static void main(String[] args) {
-
+        PasswordConfig config = parseArgs(args);
+        PasswordGenerator pg = new PasswordGenerator();
+        String password = pg.generate(config);
+        System.out.printf("Сгенерированный пароль: %s", password);
     }
 
     /**
@@ -10,18 +13,17 @@ public class Main {
      */
     public static PasswordConfig parseArgs(String[] args) {
         int length = 8;
-        boolean useDigits = false, useUpperSymbols = false, useLowerSymbols = false, useSpecialSymbols = false;
+        boolean useDigits = true, useUpperSymbols = true, useLowerSymbols = true, useSpecialSymbols = true;
 
         for (int i = 0; i < args.length; i++) {
             switch (args[i]) {
-                case "--length" -> length = Integer.parseInt(args[i]);
+                case "--length" -> length = Integer.parseInt(args[i + 1]);
                 case "--digits" -> useDigits = true;
                 case "--upper" -> useUpperSymbols = true;
                 case "--lower" -> useLowerSymbols = true;
                 case "--special" -> useSpecialSymbols = true;
             }
         }
-
         return new PasswordConfig(length, useDigits, useUpperSymbols, useLowerSymbols, useSpecialSymbols);
     }
 }
